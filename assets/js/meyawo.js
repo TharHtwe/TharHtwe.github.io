3b9b216b17cae13620c03c968b47605cc987a00e
Thar Htwe <thar.htwe@haulio.io>
2024-03-17 22:48:12 +0630

    Add skills and projects sections

--- assets/js/meyawo.js
+++ assets/js/meyawo.js
@@ -36,3 +36,27 @@ $('#nav-toggle').click(function(){
     $(this).toggleClass('is-active')
     $('ul.nav').toggleClass('show');
 });
+
+// Slider
+$('#recipeCarousel').carousel({
+    interval: 3000
+  })
+  
+  $('.carousel .carousel-item').each(function(){
+      var minPerSlide = 4;
+      var next = $(this).next();
+      if (!next.length) {
+      next = $(this).siblings(':first');
+      }
+      next.children(':first-child').clone().appendTo($(this));
+      
+      for (var i=0;i<minPerSlide;i++) {
+          next=next.next();
+          if (!next.length) {
+              next = $(this).siblings(':first');
+            }
+          
+          next.children(':first-child').clone().appendTo($(this));
+        }
+  });
+  
